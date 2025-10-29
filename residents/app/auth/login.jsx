c74d51616fee352051a2f66372ac4fe150bffde1
earlkian8 <earlkian8@gmail.com>
2025-10-29 23:31:31 +0800

    test: expo router

---
+++ residents/app/auth/login.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native"
+
+const Login = () => {
+
+    return (
+        <View>
+            <Text>What's up</Text>
+        </View>
+    )
+}
+
+export default Login
--- residents/app/index.jsx
+++ residents/app/index.jsx
@@ -1,11 +1,12 @@
 import { Text, View } from "react-native";
-
+import { Link } from "expo-router";
 export default function Index() {
   return (
     <View
     className="flex-1 justify-center items-center"
     >
       <Text className="text-5xl text-blue-500">Hello</Text>
+      <Link href={'/auth/login'}>Go to login</Link>
     </View>
   );
 }
